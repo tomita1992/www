a6db1157f190eb1b5fbacfa18e4a8992512d3bbb
cui02093920 <cui02093920@gmail.com>
2020-07-06 21:38:30 +0900

    文件上传逻辑

--- code/day-03/form_file.php
+++ code/day-03/form_file.php
@@ -1,10 +1,51 @@
 <?php 
 
+    function upload()
+    {
+        //查看是否上传文件
+        if(!(isset($_FILES['img'])))
+        {
+            $GLOBALS['message'] = "请上传文件";
+            return;
+        }
+        
+        //保存上传文件的信息
+        $img = $_FILES['img'];
+        
+        //查看上传文件信息的错误码
+        if($img['error'] != UPLOAD_ERR_OK)
+        {
+            $GLOBALS['message'] = '上传失败';
+            return;
+        }
+        
+        //上传的临时路径
+        $source = $img['tmp_name'];
+        //保存完整的文件名字
+        $target = './img/' . $img['name'];
+        //将上传的文件移动到文件储存路径
+        $moved = move_uploaded_file($source, $target);
+        
+        //查看是否移动成功
+        if(!(isset($moved)))
+        {
+            $GLOBALS['message'] = '上传失败';
+            return;
+        }
+        
+        echo '123';
+    }
+    
     if($_SERVER['REQUEST_METHOD'] === 'POST')
     {
+        
+        
         //接收文件时使用$_FILES超全局成员
         var_dump($_FILES);
+        upload();
     }
+
+    
 
 ?>
 <!-- 如何提交与接收数据 -->
@@ -21,6 +62,9 @@
         <input type="text" name = "foo">
         <input type="text" name = "bar">
         <input type="submit" value="提交"></input>
+        <?php if(isset($message)): ?>
+            <p style="color: indianred;"><?php echo $message; ?></p>
+        <?php endif ?>
     </form>
 </body>
 </html>
